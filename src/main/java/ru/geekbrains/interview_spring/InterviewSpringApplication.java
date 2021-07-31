052a6ffdd866fc0a7df4425363ed4961053a8dfd
Ekaterina Malygina <katty2007v@yandex.ru>
2021-07-31 22:49:25 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/interview_spring/InterviewSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.interview_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/interview_spring/InterviewSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.interview_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
